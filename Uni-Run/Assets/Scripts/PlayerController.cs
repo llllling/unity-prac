dcdb047e083df05cc97e41f3011713f6aa5d670f
MISEONGKIM <rlaaltjd46@naver.com>
2023-12-11 19:51:15 +0900

    feat : playerController start, die 메서드 내부 작성

--- Uni-Run/Assets/Scripts/PlayerController.cs
+++ Uni-Run/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour {
    public AudioClip deathClip; // 사망시 재생할 오디오 클립
    public float jumpForce = 700f; // 점프 힘
 
+    private const int maxJumpCount = 2;
    private int jumpCount = 0; // 누적 점프 횟수
    private bool isGrounded = false; // 바닥에 닿았는지 나타냄
    private bool isDead = false; // 사망 상태
@@ -14,15 +15,59 @@ public class PlayerController : MonoBehaviour {
    private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
 
    private void Start() {
-       // 초기화
-   }
+        // 초기화
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
 
    private void Update() {
-       // 사용자 입력을 감지하고 점프하는 처리
-   }
+        // 사용자 입력을 감지하고 점프하는 처리
+        if (isDead)
+        {
+            return;
+        }
+
+        // 마우스 왼쪽 버튼 누르고 최대 점프 횟수에 도달하지 않았으면
+        bool isJumpOn = Input.GetMouseButtonDown(0) && jumpCount < maxJumpCount;
+        //마우스 왼쪽 버튼에서 손을 떼는 순간 && 속도의 y 값이 양수라면(위로 상승 중)
+        /*
+         playerRigidbody.velocity.y > 0 : 최고점에서 속도는 제로에 가깝다. 
+            이 시점에서 점프 속도가 아닌 낙하 속도를 절반으로 줄이는 문제가 발생할 수 있다.
+            마우스 왼쪽 버튼을 너무 오래 누르고 있다가 캐릭터가 최고 높이에 도달한 후 낙하하기 시작한 시점에 손을 떼었다고 가정.
+            y 방향 속도 값이 0 이하일 때 속도를 절반으로 줄이면 상승 속도가 아니라 낙하 속도가 절반 줄어듬 그래서 해당 조건 추가
+         */
+        bool isJumpOff = Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0;
+        if (isJumpOn)
+        {
+            jumpCount++;
+            // 점프 직전 속도를 제로로 변경 => 직전까지의 힘(속도)가 상쇄되거나 합쳐져서 점프 높이가 비일관적으로 되는 현상을 막기위해
+            /*
+               1. 점프 사이에 충분한 시간 간격을 두고 이단 점프 실행(마우스 왼쪽 버튼을 여유 있게 두번 클릭)
+               2. 매우 짧은 간격으로 이단 점프 실행(마우스 왼쪽 버튼을 빠르게 두번 클릭)
+               2번의 경우 첫 번째 점프의 힘과 속력이 두 번째 점프의 힘과 속력에 그대로 합쳐진다. 따라서 2의 경우 두 번째 점프에 의한 상승 속도와 높이가 1의 경우에 비해 비약적으로 증가함
+             */
+            playerRigidbody.velocity = Vector2.zero;
+            //리비드바디의 위쪽으로 힘 주기
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            playerAudio.Play();
+        } else if (isJumpOff) {
+            //속도 절반으로 변경
+            playerRigidbody.velocity -= playerRigidbody.velocity * 0.5f;
+        }
+
+        animator.SetBool("Grounded", isGrounded);
+    }
 
    private void Die() {
-       // 사망 처리
+        // 사망 처리
+        animator.SetTrigger("Die");
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        playerRigidbody.velocity = Vector2.zero;
+        isDead = true;
    }
 
    private void OnTriggerEnter2D(Collider2D other) {
