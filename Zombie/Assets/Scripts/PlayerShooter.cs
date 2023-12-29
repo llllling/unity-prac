f6d8e2b8d1ce779841f136bf9003df2366487977
MISEONGKIM <rlaaltjd46@naver.com>
2023-12-29 16:12:20 +0900

    feat(script) : PlayerShooter onAnimatorIK 완성

--- Zombie/Assets/Scripts/PlayerShooter.cs
+++ Zombie/Assets/Scripts/PlayerShooter.cs
@@ -54,5 +54,16 @@ public class PlayerShooter : MonoBehaviour {
     private void OnAnimatorIK(int layerIndex) {
         gunPivot.position = playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
 
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandMount.rotation);
+
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, rightHandMount.rotation);
     }
 }
